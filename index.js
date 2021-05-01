b860f7d42aacfb7c2d8a29e65a3e0f94a9614496
Priya Mourya <65748264+priyamourya@users.noreply.github.com>
2021-05-01 11:57:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+
+var drumButton=document.querySelectorAll(".drum").length;
+for(var i=0 ;i<drumButton;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        if(this.style.color!="black"){
+            this.style.color="black";
+        }
+        else{
+            this.style.color="#DA0463";
+        }
+       
+        var buttonInnerHtml=this.innerHTML;
+        makesound(buttonInnerHtml);
+        press(buttonInnerHtml);
+        
+    });
+}
+addEventListener("keypress",function(event){
+    makesound(event.key);
+press(event.key);}
+);
+function makesound(key){
+    switch(key){
+        case "w":
+            var audio1=new Audio('/sounds/tom-1.mp3');
+            audio1.play();
+            break;
+        case "a":
+            var audio2=new Audio('sounds/tom-2.mp3');
+            audio2.play();
+            break;
+        case "s":
+            var audio3=new Audio('sounds/tom-3.mp3');
+            audio3.play();
+            break;
+        case "d":
+            var audio4=new Audio('sounds/tom-4.mp3');
+            audio4.play();
+            break;
+        case "j":
+            var audio5=new Audio('sounds/crash.mp3');
+            audio5.play();
+            break;
+        case "k":
+            var audio6=new Audio('sounds/kick-bass.mp3');
+            audio6.play();
+            break;
+        case "l":
+            var audio7=new Audio("sounds/snare.mp3");
+            audio7.play();
+            break;
+    }
+}
+function press(key){
+    var activeButton=document.querySelector("."+key);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },1000);
+}
